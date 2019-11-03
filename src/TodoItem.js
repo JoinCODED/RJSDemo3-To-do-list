lailalelouch/RJSDemo3

change class to functional components

src/TodoItem.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./App.css";
 
-class TableRow extends Component {
-  render() {
-    const task = this.props.task;
+function TableRow(props) {
+  const task = props.task;
 
-    const statusIcon = done => {
-      if (done) {
-        return "fa fa-check-circle";
-      } else {
-        return "fa fa-times-circle";
-      }
-    };
+  const statusIcon = done => {
+    if (done) {
+      return "fa fa-check-circle";
+    } else {
+      return "fa fa-times-circle";
+    }
+  };
 
-    return (
-      <tr className="row">
-        <td>
-          <i className={statusIcon(task.done)} />
-        </td>
-        <td>{task.task}</td>
-        <td className={task.priority}>{task.priority.toUpperCase()}</td>
-      </tr>
-    );
-  }
+  return (
+    <tr className="row">
+      <td>
+        <i className={statusIcon(task.done)} />
+      </td>
+      <td>{task.task}</td>
+      <td className={task.priority}>{task.priority.toUpperCase()}</td>
+    </tr>
+  );
 }
 
 export default TableRow;

JoinCODED/RJSDemo3-To-do-list

src/TodoItem.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./App.css";
 
-class TableRow extends Component {
-  render() {
+function TableRow(props) {
+  const item = props.item;
 
-const item = this.props.item;
-
-    const statusIcon = done => {
-      if (done) {
-        return "fa fa-check-circle";
-      } else {
-        return "fa fa-times-circle";
-      }
-    };
+  const statusIcon = done => {
+    if (done) {
+      return "fa fa-check-circle";
+    } else {
+      return "fa fa-times-circle";
+    }
+  };
 
     return (
       <tr className="row">
@@ -25,6 +23,6 @@ const item = this.props.item;
       </tr>
     );
   }
-}
+
 
 export default TableRow;